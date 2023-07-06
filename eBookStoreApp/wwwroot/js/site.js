750801386112ffc6ff8d2810283b4e2c36ea5b41
baron.trinh <baron.trinh@avepoint.com>
2023-07-06 11:07:03 +0700

    Complete ajax

--- eBookStoreApp/wwwroot/js/site.js
+++ eBookStoreApp/wwwroot/js/site.js
@@ -1,23 +1,57 @@
 ﻿function send(url, method, data, callback) {
-    //const securityToken = $('input[name="__RequestVerificationToken"]').val();
-    const token = $.cookie('AccessToken');
+    var token = $.cookie('AccessToken');
+    var event = $('#submit-btn').attr('onclick');
+    $('#submit-btn').attr('onclick', '');
+
     $.ajax({
         url: url,
         data: data,
-        headers: {
-            //"RequestVerificationToken": securityToken,
-            'Authorization': 'Bearer ' + token
-        },
+        headers: { 'Authorization': 'Bearer ' + token },
         cache: false,
         processData: false,
         type: method,
         contentType: 'application/json',
-        success: function (result) {
+
+        success: (result) => {
             callback(result);
+            $('#submit-btn').attr('onclick', event);
         },
-        error: function (err) {
+
+        error: (err) => {
             console.error(err.toString());
             callback('error');
+            $('#submit-btn').attr('onclick', event);
+        }
+    });
+}
+
+function sendDelete(relativeUrl, eventSelector, removeSelector) {
+    if (!confirm('Bạn chắc chắn muốn xóa?')) return;
+
+    var token = $.cookie('AccessToken');
+    var url = 'http://localhost:5137/' + relativeUrl;
+    var onclickEvent = $(eventSelector).attr('onclick');
+    $(eventSelector).attr('onclick', '');
+
+    $.ajax({
+        url: url,
+        headers: { 'Authorization': 'Bearer ' + token },
+        cache: false,
+        processData: false,
+        type: 'DELETE',
+
+        success: (result) => {
+            console.log(result);
+            $(removeSelector).remove();
+            $('#error').addClass('d-none');
+            $('#success').removeClass('d-none');
+        },
+
+        error: (err) => {
+            console.error(err.toString());
+            $(eventSelector).attr('onclick', onclickEvent);
+            $('#error').removeClass('d-none');
+            $('#success').addClass('d-none');
         }
     });
 }
@@ -29,10 +63,20 @@ function submitForm(relativeUrl, method) {
 }
 
 function convertFormToJSON(form) {
-    return JSON.stringify($(form)
-        .serializeArray()
+    return JSON.stringify($(form).serializeArray()
         .reduce(function (json, { name, value }) {
-            if (name != '__RequestVerificationToken') {
+            if (name == '__RequestVerificationToken') {
+                return json;
+            }
+            var type = $('[name="' + name + '"]').attr('type');
+            if (type === 'date') {
+                var dateValue = new Date(value);
+                value = dateValue.toISOString().replace('.000Z', 'Z');
+            }
+            if (type === 'number') {
+                json[name] = parseFloat(value);
+            }
+            else {
                 json[name] = value;
             }
             return json;
@@ -48,4 +92,8 @@ function commonCallback(response) {
         $('#error').addClass('d-none');
         $('#success').removeClass('d-none');
     }
+}
+
+function commonDeleteCallback(selector) {
+    $(selector).remove();
 }
